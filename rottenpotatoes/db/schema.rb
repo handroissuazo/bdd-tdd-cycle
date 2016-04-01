6963d0d2d07fc9c5faee65a02e704b89a8c42c52
alejandrojsuazo <alejandrojsuazo@tamu.edu>
2016-04-01 22:29:49 +0000

    Finalizing behavior and database

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151005214544) do
+ActiveRecord::Schema.define(version: 20151005214544) do
 
-  create_table "movies", :force => true do |t|
+  create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.string   "rating"
     t.text     "description"
